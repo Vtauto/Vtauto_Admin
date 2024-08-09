d668c34a31da91a215276788a5ef3ccde0638717
teamparmjeet <teamparmjeet@gmail.com>
2024-08-09 11:30:45 +0530

    ch

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  api: {
+    bodyParser: {
+      sizeLimit: '1000mb', // Adjust this limit as needed
+    },
+  },
 };
 
 export default nextConfig;
